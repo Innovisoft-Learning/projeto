3ff9c45955323b7a325dfd89d307a3cacc2b7fd3
Davi-Alcantara <playsd65@gmail.com>
2023-07-23 13:18:03 -0300

    Corrigi o bug do dropdown e mudei o sript do header

--- arquivos-compartilhados/js-header.js
+++ arquivos-compartilhados/js-header.js
@@ -1,27 +1,34 @@
-// JavaScript para ocultar e exibir o dropdown
+// Adicione o evento DOMContentLoaded para garantir que o código só será executado após o carregamento completo da página.
 document.addEventListener("DOMContentLoaded", function () {
-    const dropdownTrigger = document.querySelector(".cursos-nav");
-    const dropdown = document.querySelector(".submenu");
-  
-    if (dropdownTrigger && dropdown) {
-      // Ocultar o dropdown quando a página é rolada
-      document.addEventListener("scroll", function () {
-        const dropdownIsVisible = window.getComputedStyle(dropdown).display === "block";
-        if (dropdownIsVisible) {
-          dropdown.style.display = "none";
-        }
+  // Carrega o conteúdo do header.html e insere no elemento com o ID 'header-placeholder'
+  fetch('arquivos-compartilhados/header.html')
+      .then(response => response.text())
+      .then(content => {
+          const headerPlaceholder = document.getElementById('header-placeholder');
+          headerPlaceholder.innerHTML = content;
+
+          // JavaScript para ocultar e exibir o dropdown
+          const dropdownTrigger = document.querySelector(".cursos-nav");
+          const dropdown = document.querySelector(".submenu");
+
+          if (dropdownTrigger && dropdown) {
+              // Ocultar o dropdown quando a página é rolada
+              document.addEventListener("scroll", function () {
+                  const dropdownIsVisible = window.getComputedStyle(dropdown).display === "block";
+                  if (dropdownIsVisible) {
+                      dropdown.style.display = "none";
+                  }
+              });
+
+              // Exibir o dropdown quando o mouse passa sobre o item do menu
+              dropdownTrigger.addEventListener("mouseover", function () {
+                  dropdown.style.display = "block";
+              });
+
+              // Ocultar o dropdown quando o mouse sai do item do menu
+              dropdownTrigger.addEventListener("mouseout", function () {
+                  dropdown.style.display = "none";
+              });
+          }
       });
-  
-      // Exibir o dropdown quando o mouse passa sobre o item do menu
-      dropdownTrigger.addEventListener("mouseover", function () {
-        dropdown.style.display = "block";
-      });
-  
-      // Ocultar o dropdown quando o mouse sai do item do menu
-      dropdownTrigger.addEventListener("mouseout", function () {
-        dropdown.style.display = "none";
-      });
-    }
 });
-  
-  
