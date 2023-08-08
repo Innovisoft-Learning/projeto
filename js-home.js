ca6eb607a884205e873ea2e9c9efadeff615fe5b
Davi-Alcantara <playsd65@gmail.com>
2023-08-07 22:11:06 -0300

    Add da parte de código

--- js-home.js
+++ js-home.js
@@ -10,5 +10,35 @@ sr.reveal('.card', {
     rotate: {x: 0, y: 80, z: 0},
     duration: 1000
 });
-  
+
+let originalTexto = 'alert("Olá mundo!")';
+let codigoElemento = document.getElementById('codigo');
+let isTrocando = false;
+
+
+function olamundo() {
+    alert("Olá mundo!")
+    if (!isTrocando) {
+        isTrocando = true;
+        trocarCaracteres();
+        setTimeout(() => {
+            codigoElemento.textContent = originalTexto;
+            isTrocando = false;
+        }, 1000); // Tempo de troca em milissegundos (2 segundos)
+    }
+}
+
+function trocarCaracteres() {
+    let randomTexto = '';
+    for (let i = 0; i < originalTexto.length; i++) {
+        randomTexto += String.fromCharCode(Math.random() * (126 - 33) + 33); // Gera um caractere aleatório
+    }
+    codigoElemento.textContent = randomTexto;
+
+    if (isTrocando) {
+        setTimeout(trocarCaracteres, 50); // Trocar a cada 50ms
+    } else {
+        codigoElemento.textContent = originalTexto;
+    }
+}
   
