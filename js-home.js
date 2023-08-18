92b82872b6834972a7e7b3529ad111fff920d03b
Davi-Alcantara <playsd65@gmail.com>
2023-08-17 22:26:44 -0300

    Correção de erro do scrollReveal

--- js-home.js
+++ js-home.js
@@ -1,14 +1,16 @@
 
-window.sr = ScrollReveal({ reset: true });
+window.sr = ScrollReveal({ reset: false });
 
 sr.reveal('.quadrados', {
     rotate: {x: 80, y: 0, z: 0},
-    duration: 1000
+    duration: 1000,
+    delay: 500
 });
 
 sr.reveal('.card', {
     rotate: {x: 0, y: 80, z: 0},
-    duration: 1000
+    duration: 1000,
+    delay: 500
 });
 
 let originalTexto = 'alert("Olá mundo!")';
