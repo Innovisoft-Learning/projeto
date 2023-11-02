8874b09f0efd8db26d9fa08115aede2ce6e06a56
Davi-Alcantara <playsd65@gmail.com>
2023-11-02 03:00:31 -0300

    att exercicio

--- teste-ex/ex.js
+++ teste-ex/ex.js
@@ -1,5 +1,11 @@
 var slideIndex = 0;
 showSlide(slideIndex);
+let modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
+let textoModal = document.getElementById("texto-modal");
+let correcao = "none";
+
+
 
 function showSlide(n) {
     var slides = document.getElementsByClassName("slide");
@@ -31,24 +37,85 @@ function prevSlide() {
 function botao(botaoClicado) {
     let errados = document.getElementsByClassName("errados");
     let botoes = document.getElementsByClassName("botao-ex");
-    //for (let i = 0; i < errados.length; i++) {
-    //    errados[i].style.color = "red";
-    //    errados[i].style = "border-color: red; background-color: rgb(148, 0, 0);";
-
-    //}
+    modal.style.display = "block";
     console.log(botaoClicado.value)
-    if (botaoClicado.value == "A"){
-        alert("resposta certa");
+    if (botaoClicado.value == "A") {
+        console.log(botaoClicado);
+        botaoClicado.innerHTML = "✔";
+        textoModal.innerHTML = "Parabéns, você acertou! ✅";
     }
     else {
-        //alert("resposta errada");
-        botaoClicado.innerHTML = "X";
-        botaoClicado.style = "color: white;";
+        botaoClicado.innerHTML = "❌";
+        textoModal.innerHTML = "Que pena, você marcou a incorreta. ❌"
     };
 
     for (let i = 0; i < botoes.length; i++) {
         botoes[i].disabled = "true";
         botoes[i].style = "cursor: not-allowed;"
+        botaoClicado.style.color = "green";
+    }
+
+    textoModal.style.color = "black";
+
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+document.getElementById("botao-confirmar").addEventListener("click", function () {
+    var checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+    var allCheckboxes = document.querySelectorAll('input[type="checkbox"]');
+    var valoresSelecionados = [];
+    var listaCerta = ['opcao2', 'opcao4'];
+    var botaoConfirmar = document.getElementById("botao-confirmar");
+
+
+
+
+    for (var i = 0; i < checkboxes.length; i++) {
+        valoresSelecionados.push(checkboxes[i].name);
+    }
+
+    for (var i = 0; i < allCheckboxes.length; i++) {
+        allCheckboxes[i].disabled = "true";
+        allCheckboxes[i].style.cursor = "not-allowed";
+    }
+
+    if (valoresSelecionados.length == 2) {
+        for (var i = 0; i < 2; i++) {
+            if (valoresSelecionados[i] == listaCerta[i]) {
+                correcao = true;
+            }
+            else {
+                correcao = false;
+            }
+        }
+    }
+    else {
+        correcao = false;
+    }
+
+
+    modal.style.display = "block";
+    textoModal.style.color = "black";
+    if (correcao == true){
+        textoModal.innerHTML = "Parabéns, você acertou! ✅";
+    }
+
+    else {
+        textoModal.innerHTML = "Que pena, a resposta está incorreta. ❌"
     }
     
-}
+    botaoConfirmar.disabled = true;
+    botaoConfirmar.style.cursor = "not-allowed";
+
+});
