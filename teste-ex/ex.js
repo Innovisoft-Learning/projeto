9f5cfa46ba1d90f0150b677e14a2a6c42d6dc2ab
Davi-Alcantara <playsd65@gmail.com>
2023-11-01 23:41:53 -0300

    ex

--- teste-ex/ex.js
+++ teste-ex/ex.js
@@ -28,11 +28,27 @@ function prevSlide() {
 }
 
 
-function botaoErrado() {
+function botao(botaoClicado) {
     let errados = document.getElementsByClassName("errados");
-    for (let i = 0; i < errados.length; i++) {
-        errados[i].style.color = "red";
-        errados[i].style = "border-color: red; background-color: rgb(148, 0, 0);";
+    let botoes = document.getElementsByClassName("botao-ex");
+    //for (let i = 0; i < errados.length; i++) {
+    //    errados[i].style.color = "red";
+    //    errados[i].style = "border-color: red; background-color: rgb(148, 0, 0);";
 
+    //}
+    console.log(botaoClicado.value)
+    if (botaoClicado.value == "A"){
+        alert("resposta certa");
     }
+    else {
+        //alert("resposta errada");
+        botaoClicado.innerHTML = "X";
+        botaoClicado.style = "color: white;";
+    };
+
+    for (let i = 0; i < botoes.length; i++) {
+        botoes[i].disabled = "true";
+        botoes[i].style = "cursor: not-allowed;"
+    }
+    
 }
